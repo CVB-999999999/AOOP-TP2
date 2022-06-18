e76e5098f58f9c10751ad688396c76efeb9cf579
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-06-18 19:50:05 +0100

    Reg Linear

--- 2022-insolv-RegressaoLinear.py
+++ 2022-insolv-RegressaoLinear.py
@@ -6,7 +6,9 @@ from   sklearn.linear_model import LinearRegression
 from   sklearn.metrics import r2_score
 import statsmodels.api as sm
 import os
-df_dados = pd.read_csv('Insol.csv',sep=';',header=0)
+df_dados = pd.read_csv('crimes-19-m.csv',sep=',',header=0)
+
+df_dados = df_dados.fillna(0)
 
 #verificando a estrutura dos dados importados: são 200 linhas e 5 colunas
 print(df_dados.shape)
@@ -18,8 +20,8 @@ print(df_dados.describe())
 # ow()
 
 
-X = df_dados['Numero_Empregados'].values.reshape(-1,1)
-y = df_dados['Fundo_de_maneio'].values.reshape(-1,1)
+X = df_dados['Year'].values.reshape(-1,1)
+y = df_dados['IUCR'].values.reshape(-1,1)
 
 
 reg = LinearRegression()
@@ -30,23 +32,23 @@ f_previsaoes = reg.predict(X)
 
 plt.figure(figsize = (16,8))
 plt.scatter(
-    df_dados['Numero_Empregados'], 
-    df_dados['Fundo_de_maneio'], 
+    df_dados['Year'],
+    df_dados['IUCR'],
     c='red')
 plt.plot(
-    df_dados['Numero_Empregados'],
+    df_dados['Year'],
     f_previsaoes,
     c='blue',
     linewidth=3,
     linestyle=':'
 )
 
-plt.xlabel(" ($) Numero de Empregados")
-plt.ylabel(" ($) Maneio ")
+plt.xlabel(" ($) Year")
+plt.ylabel(" ($) IUCR ")
 plt.show()
 
-X = df_dados['Numero_Empregados']
-y = df_dados['Fundo_de_maneio']
+X = df_dados['Year']
+y = df_dados['IUCR']
 X2 = sm.add_constant(X)
 est = sm.OLS(y, X2)
 est2 = est.fit()
